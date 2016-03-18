56f770290026bdff66455cf32a72605d2847d2d3
softgoto <softgoto@gmail.com>
2016-03-18 11:09:22 +0800

    init

---
+++ XHLibrary/XHLibrary.h
@@ -0,0 +1,13 @@
+//
+//  XHLibrary.h
+//  XHLibrary
+//
+//  Created by xuhui on 16/3/18.
+//  Copyright © 2016年 softgoto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XHLibrary : NSObject
+
+@end
---
+++ XHLibrary/XHLibrary.m
@@ -0,0 +1,13 @@
+//
+//  XHLibrary.m
+//  XHLibrary
+//
+//  Created by xuhui on 16/3/18.
+//  Copyright © 2016年 softgoto. All rights reserved.
+//
+
+#import "XHLibrary.h"
+
+@implementation XHLibrary
+
+@end
